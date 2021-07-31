649a9ef0f7c66b1e7c4d85926ed896bdd3762a67
Anagha0718 <63951588+Anagha0718@users.noreply.github.com>
2021-07-31 22:26:40 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,36 @@
+export default  {
+    products:[{
+        _id :'123',
+        name : 'OLIVIA',
+        image : '/images/portrai.jpg',
+        price : 800,
+        brand : 'PRESIDENT',
+        rating: 4.5,
+        numReviews :10
+    },
+    {   _id:'121',
+        name : 'JHON',
+        image : '/images/789.jpg',
+        price : 450,
+        brand : 'PRESIDENT',
+        rating: 4.5,
+        numReviews :10
+    },
+    {   _id:'1210',
+        name : 'GALAND',
+        image : '/images/Pic.jpg',
+        price : 800,
+        brand : 'CEO',
+        rating: 4.5,
+        numReviews :10
+    },
+    {   _id:'1211',
+        name : 'KATHERINE',
+        image : '/images/im.jpg',
+        price : 800,
+        brand : 'CEO',
+        rating: 4.5,
+        numReviews :10
+    },
+]
+}
